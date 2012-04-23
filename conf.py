e996b23f249abae5012c4edb56ef91cd4f9815be
Caetie Ofiesh <caetie@gmail.com>
2012-04-23 14:56:41 -0400

    Edit conf page, edit dashboard topics

--- conf.py
+++ conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'FLOW'
-copyright = u'2012, Mark Tiele Westra, Caetie Oafiesh'
+copyright = u'2012, Mark Tiele Westra, Caetie Ofiesh'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
